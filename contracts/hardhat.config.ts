999a0d48f9a25dfa933e962b5bdeb9ec03a55453
feynyman <arun@mirana.tech>
2023-11-10 23:47:07 +0300

    initial hardhat config

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,20 @@
+import { config as dotenvconfig } from 'dotenv';
+dotenvconfig();
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+
+const config: HardhatUserConfig = {
+  networks: {
+    georli: {
+      url: `https://georli.infura.io/v3/${process.env.INFURA_KEY}`,
+        accounts: {
+          accounts: [process.env.PRIVATE_KEY || ''],
+        },
+    },    
+  },
+  solidity: "0.8.19",
+};
+
+export default config;
